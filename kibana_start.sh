418325f5d6f618cde6371de2e9e366d52e681f9b
kvagalex <kvagalex@yandex.ru>
2019-04-07 16:15:45 +0300

    Added new 3 startup files for elasticsearch, kibana and logstash

---
+++ kibana_start.sh
@@ -0,0 +1,2 @@
+docker run --link 87368152042f:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:6.7.1
+
---
+++ logstash_start.sh
@@ -0,0 +1,2 @@
+docker run --rm -it -e XPACK_MONITORING_ELSATICSEARCH_HOSTS=http://127.0.1.1:9200 docker.elastic.co/logstash/logstash:6.7.1
+
