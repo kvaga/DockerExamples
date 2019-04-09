19446d736941a9d5a44530b5d67520b92415faea
kvagalex <kvagalex@yandex.ru>
2019-04-09 11:30:36 +0300

    updated all startup scripts with network name and --rm option

--- kibana_start.sh
+++ kibana_start.sh
@@ -1,2 +1,3 @@
-docker run --link 87368152042f:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:6.7.1
+#docker run --link 87368152042f:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:6.7.1
+docker run --link 8b9943a50720:elasticsearch --rm --name kibana --network elk_network -p 5601:5601 docker.elastic.co/kibana/kibana:6.7.1
 
